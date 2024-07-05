53174d62d715275030028fa08c7a0868144fbe56
Karen <karen.nikoghosyan.her@gmail.com>
2024-07-05 10:28:31 +0400

    creating the frontend and connecting to backend

--- src/App.js
+++ src/App.js
@@ -13,11 +13,88 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [dappazon, setDappazon] = useState(null)
+
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
+  
+  const [item, setItem] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
+
+
+  const loadBlockchainData = async () => {
+    // Connect to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    
+    await provider.send("eth_requestAccounts", []); //
+    // const signer = provider.getSigner()             //
+    
+
+    const network = await provider.getNetwork()
+    // console.log(signer)
+
+    // Connect to smart contracts (Create JS Versions)
+    const dappazon = new ethers.Contract(
+      config[network.chainId].dappazon.address,
+      Dappazon,
+      provider
+    )
+    setDappazon(dappazon)
+
+    // Load products
+
+    const items = []
+
+    for (var i = 0; i < 9; i++) {
+      const item = await dappazon.items(i + 1)
+      items.push(item)
+    }
+
+    const electronics = items.filter((item) => item.category === 'electronics')
+    setElectronics(electronics)
+    const clothing = items.filter((item) => item.category === 'clothing')
+    setClothing(clothing)
+    const toys = items.filter((item) => item.category === 'toys')
+    setToys(toys)
+    
+
+      // 6:22
+
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
 
-      <h2>Welcome to Dappazon</h2>
+      <h2>Dappazon Best Sellers</h2>
+
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+        
+      )}
+
+      {toggle && (
+        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
+      )}
+
 
     </div>
   );
